a4de32c28687e25ebd7830d355ea3ae7c7b0f0ed
Maximiliano Delgado <mdelgado@tresdoce.com.ar>
2022-10-19 16:27:05 -0300

    chore: first commit

---
+++ packages/core/src/index.tsx
@@ -0,0 +1,8 @@
+export { theme as themeA } from './theme-a';
+export { theme as themeB } from './theme-b';
+export { Provider, ProviderProps } from './provider/index';
+export * from './Hooks';
+
+/* COMPONENTS */
+export { default as Example } from './Example';
+//-- PLOP-COMPONENTS-EXPORTS --
